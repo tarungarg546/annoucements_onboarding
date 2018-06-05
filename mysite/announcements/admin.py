5974509d51e57fc7d70549c4898736e7a44dd9db
nishanty13 <nishant.yadav@squadrun.co>
2018-06-05 12:05:12 +0530

    Added send announcement

--- mysite/announcements/admin.py
+++ mysite/announcements/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Announcements
+from django.contrib.auth.models import User
 
 class AnnouncementsAdmin(admin.ModelAdmin):
     list_display = ('title', 'message', 'date_time_expire')
-    # list_display_links = ('title', 'message')
+    change_form_template = "announcements/announcements_change_form.html"
 
+    def response_change(self, request, obj):
+        if "_send_announcement" in request.POST:
+            users = User.objects.all()
+            for user in users:
+                print str(user) + ' ' + request.POST['title'] + ' ' +request.POST['message']
+
+        return super(AnnouncementsAdmin, self).response_change(request, obj)   
 
 admin.site.register(Announcements, AnnouncementsAdmin)
